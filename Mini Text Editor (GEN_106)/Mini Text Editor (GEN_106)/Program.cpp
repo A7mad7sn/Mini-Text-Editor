6172bb7c805be465b3b4a990582e62a7d7adaa50
AhmedRedaElbhery <97313279+AhmedRedaElbhery@users.noreply.github.com>
2022-04-29 02:34:10 +0200

    update cpp file
    delete function

--- Mini Text Editor (GEN_106)/Mini Text Editor (GEN_106)/Program.cpp
+++ Mini Text Editor (GEN_106)/Mini Text Editor (GEN_106)/Program.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-file::file() 
+file::file()
 {
 }
 
@@ -21,6 +21,28 @@ string file::GetLineText(int i)
 
 void file::DeleteLine(int i)
 {
+	vector<string>::iterator it;
+	if (lines.size() != 0)
+	{
+		if (i >= 1 && i <= lines.size())
+		{
+			for (it = lines.begin() + i - 1; it <= lines.end() - 1 ; it++)
+			{
+				*it = *(it + 1);
+			}
+			lines.pop_back();
+			cout << "The line is deleted";
+		}
+		else
+		{
+			cout << "line number out of range";
+		}
+	}
+	else
+	{
+		cout << "Text File is empty";
+	}
+
 }
 
 void file::UpdateLine(int i, string l)
@@ -38,5 +60,3 @@ void file::Findandreplace(string s1, string s2)
 void file::Show()
 {
 }
-
-
